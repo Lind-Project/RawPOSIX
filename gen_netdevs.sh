17104b8a87b0ff997b0edde4841a0f0f878266a9
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2024-04-22 18:05:13 -0700

    fixed: included devcontainer (#214)
    * fixed: included devcontainer
    
    - included devcontainer
    - fixed few issues in running the tests
    - included a make target for cleanup and tests
    
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
    
    * code review comments
    
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
    
    * Add conditional compilation to fs_test
    
    ---------
    
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
    Co-authored-by: Alice W <wenyaxuan0925@outlook.com>

--- gen_netdevs.sh
+++ gen_netdevs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 WORKDIR=$(pwd)  # Set working directory to the output of pwd
 FILE="$WORKDIR/gen_netdevs"  # Use the full path for the file
-PARENTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P ) # needs the source location when being used in lind-build.
-
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
     echo "$FILE does not exist. Compiling."
-    gcc "$PARENTPATH/gen_netdevs.c" -o "$WORKDIR/gen_netdevs"
+    gcc "$WORKDIR/gen_netdevs.c" -o "$WORKDIR/gen_netdevs"
     if [ $? -ne 0 ]; then  # Check if gcc succeeded
         echo "Compilation failed."
         exit 1
