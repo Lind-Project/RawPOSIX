dce4ff5368b60df6fe6a1fbccd8868a75795c172
Justin Cappos <justincappos@gmail.com>
2024-05-16 21:21:02 -0400

    more fdtable work

--- src/example_grates/imp_grate.rs
+++ src/example_grates/imp_grate.rs
@@ -28,29 +28,29 @@ def init_circular_buffer_table() {
     // do whatever initialization I need to for the circular buffers...
 }
 
+// Get a new, unused pipe number...
+fn _get_pipe_num() {
+}
+
 // handle pipe creation
-pub fn my_pipe_syscall(cageid: u64, targetcageid: u64, callid: u64, pipefd: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {
+pub fn my_pipe_syscall(cageid: u64, targetcageid: u64, callid: u64, pipefds: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {
     // int pipe(int pipefd[2]);
 
-    let read_fd = lind_3i::reserve_fd(targetcageid,None);
-    let write_fd = lind_3i::reserve_fd(targetcageid,None);
+    let real_read_fd = lind_3i::reserve_fd(targetcageid,None);
+    let real_write_fd = lind_3i::reserve_fd(targetcageid,None);
     // size of a file descriptor in bytes...
     const FD_SIZE = 4;
 
-    // Adds an entry to the table with:
-    //      cage
-    //      fd
-    //      flags
-    //      mode
-    //      position
-    //      extra_data
-    fdtable::add_entry(targetcageid,read_fd,0,O_RDONLY,0,None);
-    fdtable::add_entry(targetcageid,write_fd,0,O_WRONLY,0,None);
+    // Adds an entry to the table.  The CLOEXEC flag is needed so we know
+    // if we need to clobber this on exec or not.
+    let mypipenum = _get_pipe_num();
+    let virt_read_pipe_fd =fdtable::get_unused_virtual_fd(targetcageid,real_read_fd,false,mypipenum);
+    let virt_write_pipe_fd = fdtable::get_unused_virtual_fd(targetcageid,real_write_fd,false,mypipenum);
 
     // Writing the fds back to the child...
-    lind_3i::write_to_cage(targetcageid,pipefd,read_fd,FD_SIZE);
-    pipefd += FD_SIZE;
-    lind_3i::write_to_cage(targetcageid,pipefd,write_fd,FD_SIZE);
+    lind_3i::write_to_cage(targetcageid,pipefds,virt_read_pipe_fd,FD_SIZE);
+    pipefds += FD_SIZE;
+    lind_3i::write_to_cage(targetcageid,pipefds,virt_write_pipe_fd,FD_SIZE);
 
     // No need to call below me...
 
@@ -147,10 +147,12 @@ pub fn exec_syscall(cageid: u64, targetcageid: u64, callid: u64, arg1: u64, arg2
 
 }
 
+// dup, etc. are handlede here as well...
+
 
 // This is the basic logic for all of the system calls that fdtable handles.
 // It decides where to route things...
-pub fn route_syscall(cageid: u64, targetcageid: u64, callid: u64, arg1: u64, arg2: u64, arg3: u64, arg4: u64, arg5: u64, arg6: u64) -> u64 {
+fn _route_syscall_or_calldown(cageid: u64, targetcageid: u64, callid: u64, arg1: u64, arg2: u64, arg3: u64, arg4: u64, arg5: u64, arg6: u64) -> u64 {
     // close(fd);
 
     if _fd_table_in(targetcageid,arg1) {
@@ -164,7 +166,7 @@ pub fn route_syscall(cageid: u64, targetcageid: u64, callid: u64, arg1: u64, arg
 
 }
 
-pub fn boilerplate_init_for_fdtables(Vec <lind_encasement::repltable>) {
+pub fn boilerplate_init_for_fdtables(Vec <lind_encasement::repltable>,enum handler) {
 
     // Let fdtable handle these (and likely I need to add others...  This 
     // most likely would be handled for me via a function call to initialize
@@ -177,6 +179,10 @@ pub fn boilerplate_init_for_fdtables(Vec <lind_encasement::repltable>) {
     // Ignore open because we don't care about files...  If we handled named
     // pipes, we would add open...
 //    syscall_replacements::push(repltable{syscallid:OPEN_SYSID, func:lind_encasement::DEFAULT});
+    if handler == CALLDOWN {
+         add _route_syscall_or_calldown to essentially all calls...
+
+    }
 
 }
 
