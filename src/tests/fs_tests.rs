cfb69ce5494b0c1cd346b8a92b5ebab46c725ae6
Alice W. <wenyaxuan0925@outlook.com>
2024-05-27 15:40:32 -0400

    sem_fork

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -1120,7 +1120,9 @@ pub mod fs_tests {
         // Attach the shared memory region
         let shmatret = cage.shmat_syscall(shmid, 0xfffff000 as *mut u8, 0);
 
-        let err = *__errno_location();
+        let err = unsafe {
+            *libc::__errno_location()
+        };
         let err_str = libc::strerror(err);
         let err_msg = unsafe {
             CStr::from_ptr(err_str).to_string_lossy().into_owned()
