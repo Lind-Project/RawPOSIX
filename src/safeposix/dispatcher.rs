a037526515a3f309eaff3ef2b898a1a8e17f126b
Alice W. <wenyaxuan0925@outlook.com>
2024-06-03 16:34:42 -0400

    chroot debug

--- src/safeposix/dispatcher.rs
+++ src/safeposix/dispatcher.rs
@@ -1102,7 +1102,7 @@ pub extern "C" fn lindrustinit(verbosity: isize) {
     // incref_root();
     // incref_root();
 
-    fs::chroot("/home/lind/lind_project/src/safeposix-rust/tmp").unwrap();
+    fs::chroot("/home/lind/lind_project/src/safeposix-rust/tmp/").unwrap();
     std::env::set_current_dir("/").unwrap();
 
     let utilcage = Cage {
