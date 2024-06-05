71eead37b8fbcfc1f8339c3c4dfcad71f251f89a
Alice W. <wenyaxuan0925@outlook.com>
2024-06-05 11:21:15 -0400

    debug fdtable

--- src/example_grates/fdtable.rs
+++ src/example_grates/fdtable.rs
@@ -293,10 +293,11 @@ pub fn copy_fdtable_for_cage(srccageid: u64, newcageid: u64) -> Result<(), three
 
     // Insert a copy and ensure it didn't exist...
     // BUG: Is this a copy!?!  Am I passing a ref to the same thing!?!?!?
-    let hmcopy = *FDTABLE.get(&srccageid).unwrap();
+    let srccage_fdtable = FDTABLE.get(&srccageid).unwrap();
+    let hmcopy = srccage_fdtable.clone();
 
     // Increment copied items
-    for entry in FDTABLE.get(&srccageid).unwrap().iter() {
+    for entry in srccage_fdtable.iter() {
         if entry.is_some() {
             let thisrealfd = entry.unwrap().realfd;
             if thisrealfd as u64 != NO_REAL_FD {
@@ -304,8 +305,15 @@ pub fn copy_fdtable_for_cage(srccageid: u64, newcageid: u64) -> Result<(), three
             }
         }
     }
-    FDTABLE.insert(newcageid, hmcopy);
-    // assert!(FDTABLE.insert(newcageid, hmcopy).is_none());
+    // FDTABLE.insert(newcageid, hmcopy);
+    println!("finish increment");
+    io::stdout().flush().unwrap();
+
+    assert!(FDTABLE.insert(newcageid, hmcopy).is_none());
+
+    println!("finish fork fdtable");
+    io::stdout().flush().unwrap();
+
     Ok(())
     // I'm not going to bother to check the number of fds used overall yet...
     //    Err(threei::Errno::EMFILE as u64),
