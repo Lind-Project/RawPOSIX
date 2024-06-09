0f68ffb75aecbb5826c310a8b22e2ca7be6fe5e1
Alice W. <wenyaxuan0925@outlook.com>
2024-06-09 19:19:29 -0400

    debug accept

--- src/safeposix/syscalls/net_calls.rs
+++ src/safeposix/syscalls/net_calls.rs
@@ -326,8 +326,9 @@ impl Cage {
                 CStr::from_ptr(err_str).to_string_lossy().into_owned()
             };
             println!("[Accept] Error message: {:?}", err_msg);
-            println!("[Accept] GenSockAddr addr: {:?}\nGenSockAddr port: {:?}\nGenSockAddr family: {:?}", debug.addr(), debug.port(), debug.get_family());
-            println!("[Accept] GenSockaddr: {:?}", finalsockaddr);
+            // println!("[Accept] GenSockAddr addr: {:?}\nGenSockAddr port: {:?}\nGenSockAddr family: {:?}", debug.addr(), debug.port(), debug.get_family());
+            println!("[Accept] NEW GenSockaddr: {:?}", inneraddrbuf);
+            println!("[Accept] GenSockaddr: {:?}", addr);
             io::stdout().flush().unwrap();
             panic!();
         }
