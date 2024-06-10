9e0b8ec8731fe7660b631ea3b2fbff07409b3033
Alice W. <wenyaxuan0925@outlook.com>
2024-06-10 17:28:13 -0400

    select

--- src/safeposix/syscalls/net_calls.rs
+++ src/safeposix/syscalls/net_calls.rs
@@ -477,7 +477,7 @@ impl Cage {
 
         // Revert result
         let (newnfds, retreadfds, retwritefds, reterrorfds) = get_virtual_bitmasks_from_select_result(
-            ret as u64,
+            nfds as u64,
             real_readfds,
             real_writefds,
             real_errorfds,
