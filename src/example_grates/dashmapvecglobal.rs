0e410d961034001495206d1cc6bcdf5de32d8552
Alice W. <wenyaxuan0925@outlook.com>
2024-06-10 15:32:05 -0400

    select

--- src/example_grates/dashmapvecglobal.rs
+++ src/example_grates/dashmapvecglobal.rs
@@ -612,7 +612,7 @@ pub fn get_virtual_bitmasks_from_select_result(nfds:u64, readbits:fd_set, writeb
         for bit in 0..nfds as usize {
             let pos = bit as u64;
             println!("[Select - FD]: _fd_isset(pos,&inset) {:?}", _fd_isset(pos,&inset));
-            println!("[Select - FD]: !_fd_isset(*mappingtable.get(&pos).unwrap(),&retbits) {:?}", !_fd_isset(*mappingtable.get(&pos).unwrap(),&retbits));
+            // println!("[Select - FD]: !_fd_isset(*mappingtable.get(&pos).unwrap(),&retbits) {:?}", !_fd_isset(*mappingtable.get(&pos).unwrap(),&retbits));
             io::stdout().flush().unwrap();
             if _fd_isset(pos,&inset)&& !_fd_isset(*mappingtable.get(&pos).unwrap(),&retbits) {
                 println!("[Select - FD]: pos {:?}", pos);
