36edba735a053b6da8bd5fcb84e297dffc250ea4
Alice W. <wenyaxuan0925@outlook.com>
2024-09-16 16:40:30 -0400

    mmap debug

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -780,10 +780,10 @@ impl Cage {
                         ((libc::mmap(addr as *mut c_void, len, prot, flags, vfd.underfd as i32, off) as i64) 
                             & 0xffffffff) as i32
                     };
-                    if ret < 0 {
-                        let errno = get_errno();
-                        return handle_errno(errno, "mmap");
-                    }
+                    // if ret < 0 {
+                    //     let errno = get_errno();
+                    //     return handle_errno(errno, "mmap");
+                    // }
                     return ret;
                 },
                 Err(_e) => {
