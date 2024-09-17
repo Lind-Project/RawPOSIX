eec4d219b8eae23099e0f46f63d34ff9cfb72bc7
Chinmay Shringi <chinmayshringi4@gmail.com>
2024-09-17 19:16:21 -0400

    fix: revert to mkdir

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -744,8 +744,7 @@ pub mod fs_tests {
 
         //Checking if removing the current working directory
         //works correctly
-        assert_eq!(cage.rmdir_syscall("/subdir1"), 0);
-        // assert_eq!(cage.mkdir_syscall("/subdir1", S_IRWXA), 0);
+        assert_eq!(cage.mkdir_syscall("/subdir1", S_IRWXA), 0);
         assert_eq!(cage.mkdir_syscall("/subdir2", S_IRWXA), 0);
         assert_eq!(cage.chdir_syscall("subdir1"), 0);
         assert_eq!(cage.rmdir_syscall("/subdir1"), 0);
