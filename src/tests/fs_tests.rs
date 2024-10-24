0dd50be16504a51cd1ac76f23c7b978a1c2033c6
lind <lind@nyu.edu>
2024-10-24 19:02:38 +0000

    fix: rm print

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -1671,7 +1671,6 @@ pub mod fs_tests {
         let _ = cage.unlink_syscall(path);
         // Create a file
         let fd = cage.open_syscall(path, O_CREAT | O_EXCL | O_WRONLY, S_IRWXA);
-        println!("fd: {}", fd);
         let mut statdata = StatData::default();
         assert_eq!(cage.stat_syscall(path, &mut statdata), 0);
         // Linkcount for the file should be 1 originally
